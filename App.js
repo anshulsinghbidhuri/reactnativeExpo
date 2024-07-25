d24402eba1975dd91a058f0894e4d347477ceb55
Anshul Singh <anshulsinghbidhuri@gmail.com>
2024-07-25 13:53:27 +0530

    Styling Solution

--- App.js
+++ App.js
@@ -2,21 +2,78 @@ import React from "react";
 import { View,Text,SafeAreaView,StyleSheet } from "react-native"; 
 const App =() => {
   return (
-    <SafeAreaView style={{flex:1}}>
-      <View style={[style.container,style.pink]}>
-        <Text>Hello World</Text>
+//     <SafeAreaView style={{flex:1}}>
+//       <View style={[style.container,style.pink]}>
+//         <Text>Hello World</Text>
+//         </View>
+//     </SafeAreaView>
+//   );
+// };
+// const style = StyleSheet.create({
+//   container:{
+//     flex:1,
+//     justifyContent:'center',
+//     alignItems:'center'
+//   },
+//   pink:{
+//     backgroundColor:'pink'
+//   }
+// });
+<SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
         </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.text}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.text}>Orange #cb4b16</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
-const style = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
   },
-  pink:{
-    backgroundColor:'pink'
-  }
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
 });
+
 export default App;

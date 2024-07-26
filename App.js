7906c407aea3e64302f3cd9905fce6c1b4ad9823
Anshul Singh <anshulsinghbidhuri@gmail.com>
2024-07-26 19:46:10 +0530

    Components

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+// npm run android
 import React from "react";
 import { View,Text,SafeAreaView,StyleSheet } from "react-native"; 
+import ColorBox from './components/ColorBox';
 const App =() => {
   return (
 //     <SafeAreaView style={{flex:1}}>
@@ -24,18 +26,10 @@ const App =() => {
         <Text style={styles.heading}>
           Here are some boxes of different colours
         </Text>
-        <View style={[styles.box, styles.cyan]}>
-          <Text style={styles.text}>Cyan #2aa198</Text>
-        </View>
-        <View style={[styles.box, styles.blue]}>
-          <Text style={styles.text}>Blue #268bd2</Text>
-        </View>
-        <View style={[styles.box, styles.magenta]}>
-          <Text style={styles.text}>Magenta #d33682</Text>
-        </View>
-        <View style={[styles.box, styles.orange]}>
-          <Text style={styles.text}>Orange #cb4b16</Text>
-        </View>
+        <ColorBox colorName="Cyan" hexCode="#2aa198"/>
+        <ColorBox colorName="Bule" hexCode="#268bd2"/>
+        <ColorBox colorName="Magenta" hexCode="#d33682"/>
+        <ColorBox colorName="Orange" hexCode="#cb4b16"/>
       </View>
     </SafeAreaView>
   );
@@ -55,25 +49,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
-  box: {
-    padding: 10,
-    borderRadius: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  cyan: {
-    backgroundColor: '#2aa198',
-  },
-  blue: {
-    backgroundColor: '#268bd2',
-  },
-  magenta: {
-    backgroundColor: '#d33682',
-  },
-  orange: {
-    backgroundColor: '#cb4b16',
-  },
+  
 });
 
 export default App;

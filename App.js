c8df729eda5684957e736a844810842b6caba50c
Anshul Singh <anshulsinghbidhuri@gmail.com>
2024-08-03 23:38:36 +0530

    Opening a Full Screen Modal

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 // npm run android
+// https://kadikraman.github.io/react-native-v2/(all the code of this course  is here with explanation)
 import 'react-native-gesture-handler';
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
@@ -6,18 +7,33 @@ import{NativeContainer}from '@react-navigation/native';
 import Home from "./Screens/Home";
 import ColorPalette from './Screens/ColorPalette';
 import { createStackNavigator } from '@react-navigation/stack';
-const Stack = createStackNavigator();
+import AddNewPaletteModal  from './Screens/AddNewPaletteModal ';
+// const Stack = createStackNavigator();
+const MainStack = createStackNavigator();
+const RootStack=createStackNavigator();
+const MainStackScreen=()=>{
+  return(
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home}/>
+      <MainStack.Screen
+      name="ColorPalette"
+      component={ColorPalette}
+      options={({route})=>({title:route.params.paletteName})}
+      />
+    </MainStack.Navigator>
+  );
+};
 const App =() => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen
-        name="ColorPalette"
-        component={ColorPalette}
-        options={({route})=>({title:route.params.paletteName})}
+        <RootStack.Navigator mode="modal">
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
         />
-      </Stack.Navigator>
+         <RootStack.Screen name="AddNewPalette" component={AddNewPaletteModal} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
